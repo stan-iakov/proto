f1743580c03d51030bb4391c078a5f7c5267d32e
stan <stan@stans-macbook-pro.home>
2020-04-11 15:50:04 +0200

    changing rate

--- server/currency.go
+++ server/currency.go
@@ -17,5 +17,5 @@ func NewCurrency(l hclog.Logger) *Currency {
 
 func (c *Currency) GetRate(ctx context.Context, rr *protos.RateRequest) (*protos.RateResponse, error) {
 	c.log.Info("handle request for GetRate", "base", rr.GetBase(), "dest", rr.GetDestination())
-	return &protos.RateResponse{Rate: 0.6}, nil
+	return &protos.RateResponse{Rate: 0.7}, nil
 }
